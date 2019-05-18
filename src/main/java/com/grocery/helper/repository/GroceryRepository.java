98c56f03b1b9e94a934af8e7c433b42202af670a
syedasadali <asad.ali@momenton.com.au>
2019-05-18 17:28:56 +1000

    First changes for Grocery Helper

---
+++ src/main/java/com/grocery/helper/repository/GroceryRepository.java
@@ -0,0 +1,14 @@
+package com.grocery.helper.repository;
+
+import com.grocery.helper.bean.Grocery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Set;
+
+@RepositoryRestResource
+public interface GroceryRepository extends JpaRepository<Grocery, Long> {
+    Page<Grocery> findByCategory(Set<String> category, Pageable pageable);
+}
---
+++ src/test/java/com/grocery/helper/controller/GroceryControllerTest.java
@@ -0,0 +1,53 @@
+package com.grocery.helper.controller;
+
+import com.grocery.helper.bean.Grocery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GroceryController.class)
+public class GroceryControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private GroceryController controller;
+
+    @MockBean
+    private Pageable pageable;
+
+
+    @Test
+    public void givenGroceries_whenGetAllGroceries_thenReturnJsonArray()
+            throws Exception {
+
+        Set<String> queryCategory = new HashSet<>();
+        queryCategory.add("testing");
+
+        Grocery grocery = Grocery.builder().id(Long.parseLong("1")).itemName("Paste").category(queryCategory).build();
+        Page<Grocery> groceryPage = new PageImpl<>(Collections.singletonList(grocery));
+        given(controller.getAllGroceries(pageable)).willReturn(groceryPage);
+
+        mvc.perform(get("/groceries")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
